b25bdcc8d1e540c30d77977a889f949e990586a5
Patrick Deutschmann <patrick.deutschmann@student.tugraz.at>
2021-04-10 20:08:25 +0200

    Add SGNN3.3: same as SGNN3.2 but doesn't use recurrency for nodes

--- train.py
+++ train.py
@@ -19,6 +19,7 @@ from model.SplitGNN_2 import SplitGNN_2
 from model.SplitGNN_3 import SplitGNN_3
 from model.SplitGNN_3_1 import SplitGNN_3_1
 from model.SplitGNN_3_2 import SplitGNN_3_2
+from model.SplitGNN_3_3 import SplitGNN_3_3
 
 import arrow
 import torch
@@ -139,6 +140,8 @@ def get_model():
         return SplitGNN_3_1(hist_len, pred_len, in_dim, city_num, batch_size, device, graph.edge_index, graph.edge_attr, wind_mean, wind_std)
     elif exp_model == 'SplitGNN_3_2':
         return SplitGNN_3_2(hist_len, pred_len, in_dim, city_num, batch_size, device, graph.edge_index, graph.edge_attr, wind_mean, wind_std)
+    elif exp_model == 'SplitGNN_3_3':
+        return SplitGNN_3_3(hist_len, pred_len, in_dim, city_num, batch_size, device, graph.edge_index, graph.edge_attr, wind_mean, wind_std)
     else:
         raise Exception('Wrong model name!')
 
